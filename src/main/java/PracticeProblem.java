80cf11e495609e5e4aa42bbb0884c8668d589753
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-25 17:10:34 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,35 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean isPalindrome(String text){
+		text = text.replaceAll(" ", "");
+		text = text.toLowerCase();
+		for (int i = 0; i < text.length()/2; i++){
+			if (text.charAt(i) != text.charAt(text.length() -1 - i))
+		return false;
+	}
+	return true;
 }
+	public static int getAge(String names[], int age[], String name){
+		for (int i = 0;i < age.length;i++){
+			if (name.equals(names[i])){
+				return age[i];
+			}
+		}
+		return -1;
+	}
+	public static int countWords(String text, char letter){
+		int wordCount = 0;
+		String [] phrase = text.split(" ");
+		for (int i = 0;i < phrase.length;i++){
+			for (int k = 0;k < phrase[i].length();k++){
+				if (phrase[i].charAt(k) == letter){
+				wordCount+=1;
+				break;
+			}
+		}
+		}
+			return wordCount;
+	}
+}
+
